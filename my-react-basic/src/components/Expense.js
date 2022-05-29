cfd3898d528305139566c09863eb0dca790c9dcc
ShengKai234 <bboykai0504@gmail.com>
2022-05-29 16:26:25 +0800

    React Basics - Practices: reusable wrapper component

--- my-react-basic/src/components/Expense.js
+++ my-react-basic/src/components/Expense.js
@@ -1,9 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import './Expense.css';
+
 
 function Expense(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem
                 title={props.expenses[0].title}
                 amount={props.expenses[0].amount}
@@ -19,7 +21,7 @@ function Expense(props) {
                 amount={props.expenses[2].amount}
                 date={props.expenses[2].date}
             />
-        </div>
+        </Card>
     );
 }
 
