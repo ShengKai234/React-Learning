8bb17216a20bbb4c0a0a013cce0109e25ae52734
kai <bboykai0504@gmail.com>
2022-06-26 01:44:34 +0800

    React State & Event - Adding form input

--- my-react-basic/src/App.js
+++ my-react-basic/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -24,6 +26,11 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense =>  {
+    console.log('In App.js');
+    console.log(expense);
+  }
+
   // return React.createElement(
   //   'div',
   //   {},
@@ -32,7 +39,7 @@ function App() {
   // );
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses}/>
     </div>
   );
---
+++ my-react-basic/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        
+        // this is not good approuch in same time update value.
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+
+        // in this inner function, will always be the latest state snapshot.
+        // this is save way to ensure that you always operate on the latest state snapshot.
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // })
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
+    }
+
+    const submitHandler = (event) => {
+        // this is default JavaScript behavior
+        // prevent the default of this request being sent and since that request is not sent
+        // the page will now also not reload
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    }
+
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type='number' value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type='date' value={enteredDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    )
+}
+
+export default ExpenseForm;
