4c62259f0687ae79ec5d710a4b1663fcacae8196
ShengKai234 <bboykai0504@gmail.com>
2022-07-02 03:00:28 +0800

    Rendering Lists & Conditional Content part 2

--- my-react-basic/src/components/Expenses/Expenses.js
+++ my-react-basic/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import './Expenses.css';
+import ExpensesList from "./ExpensesList";
 
 
 function Expense(props) {
@@ -21,14 +21,7 @@ function Expense(props) {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date}
-                    />
-                ))}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     );
